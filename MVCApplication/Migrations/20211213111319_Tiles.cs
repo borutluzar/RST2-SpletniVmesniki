d94df24ed588cae780723e267f2d79eea6b05abb
Borut Lužar <borut.luzar@gmail.com>
2021-12-15 15:49:49 +0100

    Added MVC project

---
+++ MVCApplication/Migrations/20211213111319_Tiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCApplication.Migrations
+{
+    public partial class Tiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tiles",
+                columns: table => new
+                {
+                    TileID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateInserted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Thumb = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tiles", x => x.TileID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tiles");
+        }
+    }
+}
---
+++ MVCApplication/Models/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.Models
+{
+    public class WebAppContext : DbContext
+    {
+        /// <summary>
+        /// Pripravimo konstruktor, ki pošlje predniku možnosti
+        /// </summary>
+        public WebAppContext(DbContextOptions<WebAppContext> options) : base(options) { }
+
+        public DbSet<Tile> Tiles { get; set; }
+    }
+}
