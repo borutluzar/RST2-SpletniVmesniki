03586b5e4d683257b3e9adfe5edc14756826c8c0
Borut Lužar <borut.luzar@gmail.com>
2021-12-15 17:25:47 +0100

    Konec

---
+++ MVCProject/Controllers/TilesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCProject;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class TilesController : Controller
+    {
+        private readonly MvcDbContext _context;
+
+        public TilesController(MvcDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tiles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tiles.ToListAsync());
+        }
+
+        // GET: Tiles/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tile = await _context.Tiles
+                .FirstOrDefaultAsync(m => m.TileID == id);
+            if (tile == null)
+            {
+                return NotFound();
+            }
+
+            return View(tile);
+        }
+
+        // GET: Tiles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TileID,Title,Content,Description,DateInserted")] Tile tile)
+        {
+            if (ModelState.IsValid)
+            {
+                tile.TileID = Guid.NewGuid();
+                _context.Add(tile);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tile);
+        }
+
+        // GET: Tiles/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tile = await _context.Tiles.FindAsync(id);
+            if (tile == null)
+            {
+                return NotFound();
+            }
+            return View(tile);
+        }
+
+        // POST: Tiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("TileID,Title,Content,Description,DateInserted")] Tile tile)
+        {
+            if (id != tile.TileID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TileExists(tile.TileID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tile);
+        }
+
+        // GET: Tiles/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tile = await _context.Tiles
+                .FirstOrDefaultAsync(m => m.TileID == id);
+            if (tile == null)
+            {
+                return NotFound();
+            }
+
+            return View(tile);
+        }
+
+        // POST: Tiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var tile = await _context.Tiles.FindAsync(id);
+            _context.Tiles.Remove(tile);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TileExists(Guid id)
+        {
+            return _context.Tiles.Any(e => e.TileID == id);
+        }
+    }
+}
---
+++ MVCProject/Migrations/20211215155647_TilesInit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCProject.Migrations
+{
+    public partial class TilesInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tiles",
+                columns: table => new
+                {
+                    TileID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateInserted = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tiles", x => x.TileID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tiles");
+        }
+    }
+}
